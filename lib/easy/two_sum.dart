b64ce1af4c3e4d55a316d02fe55e4b01c18adb33
fartem <jaman.smlnsk@gmail.com>
2023-02-02 22:05:32 +0300

    2023-02-02 update: added "1. Two Sum"

---
+++ lib/easy/two_sum.dart
@@ -0,0 +1,26 @@
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    final indices = <int, List<int>>{};
+    for (int i = 0; i < nums.length; i++) {
+      var num = nums[i];
+      if (indices.containsKey(num)) {
+        indices[num]!.add(i);
+      } else {
+        indices[num] = [i];
+      }
+    }
+    for (int i = 0; i < nums.length; i++) {
+      var num = nums[i];
+      var diff = target - num;
+      if (indices.containsKey(diff)) {
+        var ind = indices[diff]!;
+        for (int index in ind) {
+          if (index != i) {
+            return [i, index];
+          }
+        }
+      }
+    }
+    return [];
+  }
+}
