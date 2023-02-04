1ef8ae6320fbc27894543b9b8c590ee279a84c8f
fartem <jaman.smlnsk@gmail.com>
2023-02-04 17:59:55 +0300

    2023-02-04 update: added "13. Roman to Integer"

---
+++ lib/easy/roman_to_integer.dart
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/roman-to-integer/
+class Solution {
+  int romanToInt(String s) {
+    final values = <String, int>{
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000,
+    };
+    int result = 0;
+    for (int i = s.length - 1; i >= 0; i--) {
+      int curr = values[s[i]]!;
+      if (curr * 4 < result) {
+        result -= curr;
+      } else {
+        result += curr;
+      }
+    }
+    return result;
+  }
+}
