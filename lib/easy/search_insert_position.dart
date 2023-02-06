f2b8c538c6f2f69639b9472e856d3ca6b71af061
fartem <jaman.smlnsk@gmail.com>
2023-02-06 10:21:40 +0300

    2023-02-06 update: added "35. Search Insert Position"

---
+++ lib/easy/search_insert_position.dart
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/search-insert-position/description/
+class Solution {
+  int searchInsert(List<int> nums, int target) {
+    if (nums[nums.length - 1] < target) {
+      return nums.length;
+    }
+    var l = 0;
+    var r = nums.length - 1;
+    while (l < r) {
+      var m = l + (r - l) ~/ 2;
+      if (nums[m] < target) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
+    }
+    return l;
+  }
+}
